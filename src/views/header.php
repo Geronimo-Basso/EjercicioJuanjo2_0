71dbe7886c28f1ee5ce5045041dde03cda0b33a6
Juan José González Alanís <juanjo.goal@gmail.com>
2022-05-18 12:58:23 +0200

    Renamed file index.css to styles.css and moved footer.css styles to the main CSS file

--- src/views/header.php
+++ src/views/header.php
@@ -11,8 +11,7 @@
     <meta charset="UTF-8">
     <title><?php echo $title; ?></title>
 
-	<link rel="stylesheet" href="../../assets/css/index.css">
-	<link rel="stylesheet" href="../../assets/css/footer.css">
+	<link rel="stylesheet" href="../../assets/css/styles.css">
 	<link rel="stylesheet" href="../../assets/css/backend.css">
 </head>
 <body>
