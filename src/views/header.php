cbb25f683891f62aa1701a7bea920afd8ea3d41e
GeronimoBasso <gbasso@correo.um.edu.uy>
2022-05-22 17:57:36 +0200

    -New funtion use in header.php to import .css files.

--- src/views/header.php
+++ src/views/header.php
@@ -1,17 +1,31 @@
 <?php
 /**
  * Template vars.
- *
  * @var string $title The page title.
  */
+function importFile($filename){
+    ?>
+    <link rel="stylesheet" href="../../assets/css/<?php $filename?>">
+    <?php
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <title><?php echo $title; ?></title>
-
-	<link rel="stylesheet" href="../../assets/css/styles.css">
-	<link rel="stylesheet" href="../../assets/css/backend.css">
+    <?php
+    importFile('styles.css');
+    importFile('backend.css');
+    ?>
 </head>
 <body>
+
+
+
+
+
+
+
+
+
