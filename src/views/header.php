9ff5b27f55550a52889b96d53982e6692ba32498
Geronimo-Basso <gerobasso01@gmail.com>
2022-06-22 09:05:10 -0300

    commit message

--- src/views/header.php
+++ src/views/header.php
@@ -11,5 +11,13 @@
     <meta charset="UTF-8">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="<?php echo get_style_url( 'styles.css' ); ?>" />
+    <div class="menu-bar">
+        <ul>
+            <li class="active"> <a href="#"> Home </a></li>
+            <li> <a href="#"> Subscriber page </a></li>
+            <li> <a href="#"> Show More </a> </li>
+            <li> <a href="#"> About Us </a> </li>
+        </ul>
+    </div>
 </head>
 <body>
